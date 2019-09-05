f3871fe558a601856283c0de6b1bd83ffccb04bd
thule <lunthu@gmail.com>
2019-09-05 16:00:16 +0700

    devise Omiauth facebook

--- Gemfile
+++ Gemfile
@@ -23,6 +23,8 @@ gem 'image_processing', '~> 1.2'
 gem 'devise'
 gem 'jquery-rails'
 gem 'font-awesome-rails'
+gem 'omniauth'
+gem 'omniauth-facebook'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,24 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  #  :lockable, :timeoutable, :trackable and :omniauthable
+  #  :lockable, :timeoutable, :trackable and :omniauthable, :confirmable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable
+         :omniauthable, omniauth_providers: %i[facebook]
+
 
   has_one_attached :image
+
+    def self.from_omniauth(auth)
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.email = auth.info.email
+        user.password = Devise.friendly_token[0, 20]
+        user.name = auth.info.name   # assuming the user model has a name
+        user.image = auth.info.image # assuming the user model has an image
+        # If you are using confirmable and the provider(s) you use validate emails,
+        # uncomment the line below to skip the confirmation emails.
+        # user.skip_confirmation!
+        user.uid = auth.uid
+        user.provider = auth.provider
+      end
+    end
 end
